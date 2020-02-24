5289d96629e76cbe906168d30ed688f349ca794e
Dan Munro <dan@danmunro.com>
2020-02-24 06:29:53 -0600

    test and lint

---
+++ src/test/kotlin/kotlinmud/action/impl/InventoryTest.kt
@@ -0,0 +1,26 @@
+package kotlinmud.action.impl
+
+import assertk.assertThat
+import assertk.assertions.contains
+import kotlinmud.test.createTestService
+import org.junit.Test
+
+class InventoryTest {
+    @Test
+    fun testInventoryIsDisplayed() {
+        // setup
+        val testService = createTestService()
+        val mob = testService.createMob()
+
+        // given
+        val item1 = testService.createItem(mob.inventory)
+        val item2 = testService.createItem(mob.inventory)
+
+        // when
+        val response = testService.runAction(mob, "inv")
+
+        // then
+        assertThat(response.message.toActionCreator).contains(item1.name)
+        assertThat(response.message.toActionCreator).contains(item2.name)
+    }
+}
