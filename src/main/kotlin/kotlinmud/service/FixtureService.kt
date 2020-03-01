b88c3bd259d280f1fbc3d25b7f7f192637454fef
Dan Munro <dan@danmunro.com>
2020-02-29 20:44:41 -0600

    remove unused code paths

--- src/main/kotlin/kotlinmud/service/FixtureService.kt
+++ src/main/kotlin/kotlinmud/service/FixtureService.kt
@@ -10,15 +10,8 @@ import kotlinmud.mob.SpecializationType
 import kotlinmud.mob.race.Race
 import kotlinmud.mob.race.impl.Human
 import kotlinmud.reset.MobReset
-import kotlinmud.room.Direction
-import kotlinmud.room.Room
-import kotlinmud.room.exit.Door
-import kotlinmud.room.exit.DoorDisposition
-import kotlinmud.room.exit.Exit
-import kotlinmud.room.oppositeDirection
 
 class FixtureService {
-    private var rooms = 0
     private var mobs = 0
     private var items = 0
     private val faker = Faker()
@@ -27,22 +20,6 @@ class FixtureService {
         mobService.addMobReset(MobReset(createMob(), mobService.getStartRoom(), 1, 1))
         mobService.respawnWorld()
         createItem(mobService.getStartRoom().inventory)
-    }
-
-    fun generateWorld(): List<Room> {
-        val room1 = createRoom()
-        val room2 = createRoom()
-        val room3 = createRoom()
-        val room4 = createRoom()
-        createExit(room1, room2, Direction.NORTH)
-        createExit(room1, room3, Direction.SOUTH)
-        createExit(
-            room1,
-            room4,
-            Direction.EAST,
-            Door(1, "a solid wooden door", "A heavy wooden door is here.", DoorDisposition.CLOSED))
-        createItem(room1.inventory)
-        return listOf(room1, room2, room3, room4)
     }
 
     fun createMob(race: Race = Human(), specialization: SpecializationType = SpecializationType.NONE): Mob {
@@ -76,22 +53,5 @@ class FixtureService {
             attributes)
         inv.items.add(item)
         return item
-    }
-
-    private fun createExit(src: Room, dest: Room, dir: Direction, door: Door? = null): Exit {
-        val exit = Exit(dest, dir, door)
-        src.exits.add(exit)
-        dest.exits.add(Exit(src, oppositeDirection(dir), door))
-        return exit
-    }
-
-    private fun createRoom(): Room {
-        rooms++
-        return Room(
-                rooms,
-                "test room no. $rooms",
-                "a test room is here",
-                Inventory(),
-                mutableListOf())
     }
 }
