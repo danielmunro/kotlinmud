8336359bb7acacbfff9b7e6d07e3a6af2422460d
Dan Munro <dan@danmunro.com>
2020-03-18 20:03:06 -0500

    load item ac

--- src/main/kotlin/kotlinmud/loader/loader/ItemLoader.kt
+++ src/main/kotlin/kotlinmud/loader/loader/ItemLoader.kt
@@ -1,5 +1,6 @@
 package kotlinmud.loader.loader
 
+import kotlinmud.attributes.Attribute
 import kotlinmud.item.Item
 import kotlinmud.item.Material
 import kotlinmud.item.Position
@@ -11,6 +12,10 @@ class ItemLoader(private val tokenizer: Tokenizer) : Loader {
     var name = ""
     var description = ""
     var value = 0
+    var acSlash = 0
+    var acBash = 0
+    var acPierce = 0
+    var acMagic = 0
     override var props: Map<String, String> = mapOf()
 
     override fun load(): Model {
@@ -18,12 +23,21 @@ class ItemLoader(private val tokenizer: Tokenizer) : Loader {
         name = tokenizer.parseString()
         description = tokenizer.parseString()
         props = tokenizer.parseProperties()
+        val ac = strAttr("ac", "0-0-0-0").split("-")
+        acSlash = ac[0].toInt()
+        acBash = ac[1].toInt()
+        acPierce = ac[2].toInt()
+        acMagic = ac[3].toInt()
 
         return Item.Builder(id, name)
             .setDescription(description)
             .setValue(props["value"]?.toInt() ?: 1)
             .setLevel(props["level"]?.toInt() ?: 1)
             .setWeight(props["weight"]?.toDouble() ?: 1.0)
+            .setAttribute(Attribute.AC_SLASH, acSlash)
+            .setAttribute(Attribute.AC_PIERCE, acPierce)
+            .setAttribute(Attribute.AC_BASH, acBash)
+            .setAttribute(Attribute.AC_MAGIC, acMagic)
             .setMaterial(Material.valueOf(strAttr("material", "organic").toUpperCase()))
             .setPosition(Position.valueOf(strAttr("position", "none").toUpperCase()))
     }
---
+++ src/test/kotlin/kotlinmud/loader/loader/MobLoaderTest.kt
@@ -0,0 +1,22 @@
+package kotlinmud.loader.loader
+
+import assertk.assertThat
+import assertk.assertions.isGreaterThan
+import assertk.assertions.isLessThan
+import kotlinmud.loader.AreaLoader
+import org.junit.Test
+
+class MobLoaderTest {
+    @Test
+    fun testMobLoaderLoadsRandomGold() {
+        // setup
+        val area = AreaLoader("areas/midgard").load()
+
+        // given
+        val mob = area.mobs.find { it.id == 9 }!!
+
+        // expect
+        assertThat(mob.gold).isGreaterThan(0)
+        assertThat(mob.gold).isLessThan(4)
+    }
+}
