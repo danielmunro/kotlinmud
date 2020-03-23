b5a984894e77203be79775cdcd85b0a0fbe03e3a
Dan Munro <dan@danmunro.com>
2020-03-23 07:51:39 -0500

    make these data classes as well

--- src/main/kotlin/kotlinmud/loader/model/reset/ItemMobReset.kt
+++ src/main/kotlin/kotlinmud/loader/model/reset/ItemMobReset.kt
@@ -3,4 +3,4 @@ package kotlinmud.loader.model.reset
 import kotlinmud.data.Row
 import kotlinmud.loader.model.Model
 
-class ItemMobReset(override val id: Int, val itemId: Int, val mobId: Int, val maxInInventory: Int, val maxInWorld: Int) : Model, Row
+data class ItemMobReset(override val id: Int, val itemId: Int, val mobId: Int, val maxInInventory: Int, val maxInWorld: Int) : Model, Row
--- src/main/kotlin/kotlinmud/loader/model/reset/ItemRoomReset.kt
+++ src/main/kotlin/kotlinmud/loader/model/reset/ItemRoomReset.kt
@@ -3,5 +3,5 @@ package kotlinmud.loader.model.reset
 import kotlinmud.data.Row
 import kotlinmud.loader.model.Model
 
-class ItemRoomReset(override val id: Int, val itemId: Int, val roomId: Int, val maxInInventory: Int, val maxInWorld: Int) : Model,
+data class ItemRoomReset(override val id: Int, val itemId: Int, val roomId: Int, val maxInInventory: Int, val maxInWorld: Int) : Model,
     Row
