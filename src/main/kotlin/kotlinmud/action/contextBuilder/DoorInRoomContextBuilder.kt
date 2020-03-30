3e1dc2e8fb5a70baf61f1453606e030a876927b2
Dan Munro <dan@danmunro.com>
2020-03-29 21:30:01 -0500

    can open by direction

--- src/main/kotlin/kotlinmud/action/contextBuilder/DoorInRoomContextBuilder.kt
+++ src/main/kotlin/kotlinmud/action/contextBuilder/DoorInRoomContextBuilder.kt
@@ -4,11 +4,26 @@ import kotlinmud.action.Context
 import kotlinmud.action.Status
 import kotlinmud.io.Syntax
 import kotlinmud.room.Room
+import kotlinmud.room.exit.Exit
+import kotlinmud.string.matches
 
 class DoorInRoomContextBuilder(private val room: Room) : ContextBuilder {
     override fun build(syntax: Syntax, word: String): Context<Any> {
-        return room.exits.find { exit -> exit.door != null && kotlinmud.string.matches(exit.door.name, word) }
-            ?.let { Context<Any>(syntax, Status.OK, it.door!!) }
+        return room.exits.find {
+            it.door != null && matchExit(it, word)
+        }?.let { Context<Any>(syntax, Status.OK, it.door!!) }
             ?: Context<Any>(syntax, Status.FAILED, "you don't see that anywhere.")
     }
+}
+
+fun matchExit(exit: Exit, word: String): Boolean {
+    if (matches(exit.door!!.name, word)) {
+        return true
+    }
+
+    if (matches(exit.direction.value, word)) {
+        return true
+    }
+
+    return false
 }
