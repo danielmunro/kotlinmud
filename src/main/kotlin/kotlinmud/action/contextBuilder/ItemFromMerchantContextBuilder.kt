4fdd3b8f4283760374c52dff3babfbfbc3475f5a
Dan Munro <dan@danmunro.com>
2020-04-05 07:33:22 -0500

    incremental item service

--- src/main/kotlin/kotlinmud/action/contextBuilder/ItemFromMerchantContextBuilder.kt
+++ src/main/kotlin/kotlinmud/action/contextBuilder/ItemFromMerchantContextBuilder.kt
@@ -5,12 +5,17 @@ import kotlinmud.action.Status
 import kotlinmud.io.Syntax
 import kotlinmud.mob.JobType
 import kotlinmud.mob.Mob
+import kotlinmud.service.ItemService
 
-class ItemFromMerchantContextBuilder(private val mob: Mob, private val mobsInRoom: List<Mob>) : ContextBuilder {
+class ItemFromMerchantContextBuilder(
+    private val itemService: ItemService,
+    private val mob: Mob,
+    private val mobsInRoom: List<Mob>
+) : ContextBuilder {
     override fun build(syntax: Syntax, word: String): Context<Any> {
         val shopkeeper = mobsInRoom.find { it.job == JobType.SHOPKEEPER }
             ?: return Context(Syntax.ITEM_FROM_MERCHANT, Status.ERROR, "no merchant is here.")
-        val item = shopkeeper.inventory.items.find { kotlinmud.string.matches(it.name, word) }
+        val item = itemService.findByOwner(shopkeeper, word)
             ?: return Context(Syntax.ITEM_FROM_MERCHANT, Status.ERROR, "they don't have anything like that.")
         if (item.worth > mob.gold) {
             return Context(Syntax.ITEM_FROM_MERCHANT, Status.ERROR, "you can't afford that.")
--- src/main/kotlin/kotlinmud/action/contextBuilder/ItemToSellContextBuilder.kt
+++ src/main/kotlin/kotlinmud/action/contextBuilder/ItemToSellContextBuilder.kt
@@ -5,12 +5,17 @@ import kotlinmud.action.Status
 import kotlinmud.io.Syntax
 import kotlinmud.mob.JobType
 import kotlinmud.mob.Mob
+import kotlinmud.service.ItemService
 
-class ItemToSellContextBuilder(private val mob: Mob, private val mobsInRoom: List<Mob>) : ContextBuilder {
+class ItemToSellContextBuilder(
+    private val itemService: ItemService,
+    private val mob: Mob,
+    private val mobsInRoom: List<Mob>
+) : ContextBuilder {
     override fun build(syntax: Syntax, word: String): Context<Any> {
         val shopkeeper = mobsInRoom.find { it.job == JobType.SHOPKEEPER }
             ?: return Context(Syntax.ITEM_TO_SELL, Status.ERROR, "no merchant is here.")
-        val item = mob.inventory.items.find { kotlinmud.string.matches(it.name, word) }
+        val item = itemService.findByOwner(mob, word)
             ?: return Context(Syntax.ITEM_TO_SELL, Status.ERROR, "you don't have anything like that.")
         if (item.worth > shopkeeper.gold) {
             return Context(Syntax.ITEM_TO_SELL, Status.ERROR, "they can't afford that.")
