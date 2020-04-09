0163434eaf585757ec48694cc9eeeec04f6552b6
Dan Munro <dan@danmunro.com>
2020-04-08 20:18:24 -0500

    item attribute writer

--- src/main/kotlin/kotlinmud/saver/mapper/ItemMapper.kt
+++ src/main/kotlin/kotlinmud/saver/mapper/ItemMapper.kt
@@ -25,8 +25,16 @@ fun generateProps(item: Item): String {
             optional(item.drink != Drink.NONE, "drink: ${item.drink}") +
             optional(item.food != Food.NONE, "food: ${item.food}") +
             optional(item.quantity > 0, "quantity: ${item.quantity}") +
+            optional(attr.hp > 0, "hp: ${attr.hp}") +
+            optional(attr.mana > 0, "mana: ${attr.mana}") +
+            optional(attr.mv > 0, "mv: ${attr.mv}") +
             optional(attr.hit > 0, "hit: ${attr.hit}") +
             optional(attr.dam > 0, "dam: ${attr.dam}") +
+            optional(attr.strength > 0, "str: ${attr.strength}") +
+            optional(attr.intelligence > 0, "int: ${attr.intelligence}") +
+            optional(attr.wisdom > 0, "wis: ${attr.wisdom}") +
+            optional(attr.dexterity > 0, "dex: ${attr.dexterity}") +
+            optional(attr.constitution > 0, "con: ${attr.constitution}") +
             optional(attr.hasAC(), "ac: ${attr.acBash}-${attr.acSlash}-${attr.acPierce}-${attr.acMagic}")
 }
 
