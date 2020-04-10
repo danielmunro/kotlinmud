81be8f005a039d9574f8f75fe69294c17d80c2b4
Dan Munro <dan@danmunro.com>
2020-04-10 08:02:26 -0500

    one more reference

--- src/main/kotlin/kotlinmud/time/eventually.kt
+++ src/main/kotlin/kotlinmud/time/eventually.kt
@@ -4,7 +4,7 @@ import java.util.Random
 import java.util.Timer
 import java.util.TimerTask
 import kotlin.concurrent.schedule
-import kotlinmud.io.TICK_LENGTH_IN_SECONDS
+import kotlinmud.service.TICK_LENGTH_IN_SECONDS
 
 fun eventually(doThis: TimerTask.() -> Unit) {
     Timer()
