d6a34fb1d1b594263f661bde74aab148ce932097
Dan Munro <dan@danmunro.com>
2020-04-10 07:58:21 -0500

    remove old client/server

--- src/main/kotlin/kotlinmud/event/observer/impl/RemoveMobOnClientDisconnectObserver.kt
+++ src/main/kotlin/kotlinmud/event/observer/impl/RemoveMobOnClientDisconnectObserver.kt
@@ -4,16 +4,16 @@ import kotlinmud.event.Event
 import kotlinmud.event.EventResponse
 import kotlinmud.event.EventType
 import kotlinmud.event.observer.Observer
-import kotlinmud.io.ClientHandler
+import kotlinmud.io.NIOClient
 import kotlinmud.service.MobService
 
 class RemoveMobOnClientDisconnectObserver(private val mobService: MobService) : Observer {
     override val eventTypes: List<EventType> = listOf(EventType.CLIENT_DISCONNECTED)
 
     override fun <T, A> processEvent(event: Event<T>): EventResponse<A> {
-        val client = event.subject as ClientHandler
+        val client = event.subject as NIOClient
         println("remove mob ${client.mob}")
-        mobService.removeMob(client.mob)
+        mobService.removeMob(client.mob!!)
 
         @Suppress("UNCHECKED_CAST")
         return EventResponse(event.subject as A)
