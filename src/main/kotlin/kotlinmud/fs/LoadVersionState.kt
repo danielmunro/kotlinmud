81279016bd6d97daf29c46f0623eaed7d3043904
Dan Munro <dan@danmunro.com>
2020-05-01 11:30:00 -0700

    migration #1, step 1: bump write version, change format

--- src/main/kotlin/kotlinmud/fs/LoadVersionState.kt
+++ src/main/kotlin/kotlinmud/fs/LoadVersionState.kt
@@ -3,13 +3,19 @@ package kotlinmud.fs
 import java.io.File
 import kotlinmud.fs.loader.Tokenizer
 
+const val BUMP_LOAD_VERSION = false
+const val BUMP_WRITE_VERSION = true
+
 fun loadVersionState(isTest: Boolean = false): List<Int> {
     return if (File(VERSION_FILE).exists() && !isTest) {
         println("loading version state")
         val tokenizer = Tokenizer(File(VERSION_FILE).readText())
         val loadVersion = tokenizer.parseInt()
         val writeVersion = tokenizer.parseInt()
-        listOf(loadVersion, writeVersion)
+        listOf(
+            loadVersion + if (BUMP_LOAD_VERSION) 1 else 0,
+            writeVersion + if (BUMP_WRITE_VERSION) 1 else 0
+        )
     } else {
         listOf(1, 1)
     }
