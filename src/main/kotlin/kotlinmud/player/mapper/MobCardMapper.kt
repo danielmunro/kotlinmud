d0a08d3f1761b8358c6cfa342634226d8e61313f
Dan Munro <dan@danmunro.com>
2020-05-25 21:47:25 -0700

    use helper method

--- src/main/kotlin/kotlinmud/player/mapper/MobCardMapper.kt
+++ src/main/kotlin/kotlinmud/player/mapper/MobCardMapper.kt
@@ -1,20 +1,22 @@
 package kotlinmud.player.mapper
 
+import kotlinmud.fs.int
+import kotlinmud.fs.str
 import kotlinmud.player.model.MobCard
 
 fun mapMobCard(mobCard: MobCard): String {
-    return """${mobCard.playerEmail}~
-${mobCard.mobName}~
-#${mobCard.experience}
-#${mobCard.experiencePerLevel}
-#${mobCard.sacPoints}
-#${mobCard.trains}
-#${mobCard.practices}
-#${mobCard.appetite.maxHunger}
-#${mobCard.appetite.maxThirst}
-#${mobCard.appetite.getHunger()}
-#${mobCard.appetite.getThirst()}
-#${mobCard.bounty}
+    return """${str(mobCard.playerEmail)}
+${str(mobCard.mobName)}
+${int(mobCard.experience)}
+${int(mobCard.experiencePerLevel)}
+${int(mobCard.sacPoints)}
+${int(mobCard.trains)}
+${int(mobCard.practices)}
+${int(mobCard.appetite.maxHunger)}
+${int(mobCard.appetite.maxThirst)}
+${int(mobCard.appetite.getHunger())}
+${int(mobCard.appetite.getThirst())}
+${int(mobCard.bounty)}
 str: ${mobCard.attributes.strength}, int: ${mobCard.attributes.intelligence}, wis: ${mobCard.attributes.wisdom}, dex: ${mobCard.attributes.dexterity}, con: ${mobCard.attributes.constitution}, hp: ${mobCard.attributes.hp}, mana: ${mobCard.attributes.mana}, mv: ${mobCard.attributes.mv}, hit: ${mobCard.attributes.hit}, dam: ${mobCard.attributes.dam}, acSlash: ${mobCard.attributes.acSlash}, acBash: ${mobCard.attributes.acBash}, acPierce: ${mobCard.attributes.acPierce}, acMagic: ${mobCard.attributes.acMagic}~
 """
 }
--- src/main/kotlin/kotlinmud/player/mapper/PlayerMapper.kt
+++ src/main/kotlin/kotlinmud/player/mapper/PlayerMapper.kt
@@ -1,11 +1,12 @@
 package kotlinmud.player.mapper
 
+import kotlinmud.fs.str
 import kotlinmud.player.model.Player
 
 fun mapPlayer(player: Player): String {
-    return """${player.email}~
-${player.name}~
-${player.created}~
-${player.mobs.joinToString(",")}~
+    return """${str(player.email)}
+${str(player.name)}
+${str(player.created.toString())}
+${str(player.mobs.joinToString(","))}
 """
 }
