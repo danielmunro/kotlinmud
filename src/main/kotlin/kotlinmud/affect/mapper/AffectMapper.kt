8bca1632cb5537f461be1cd30d39cc23b5aeb1e0
Dan Munro <dan@danmunro.com>
2020-06-01 21:37:01 -0700

    join with newline

--- src/main/kotlin/kotlinmud/affect/mapper/AffectMapper.kt
+++ src/main/kotlin/kotlinmud/affect/mapper/AffectMapper.kt
@@ -5,7 +5,7 @@ import kotlinmud.fs.end
 import kotlinmud.fs.str
 
 fun mapAffects(affects: List<AffectInstance>): String {
-    return affects.joinToString {
+    return affects.joinToString("\n") {
         str("${it.affectType} ${it.timeout}")
     } + end()
 }
