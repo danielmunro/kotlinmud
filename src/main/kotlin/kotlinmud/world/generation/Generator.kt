733f33f84eeca7308af1fd4dcee31fae40fe76e4
Dan Munro <dan@danmunro.com>
2020-06-05 06:28:57 -0700

    incremental improvements on generator

--- src/main/kotlin/kotlinmud/world/generation/Generator.kt
+++ src/main/kotlin/kotlinmud/world/generation/Generator.kt
@@ -10,6 +10,8 @@ typealias Blocks = Array<Array<IntArray>>
 typealias Layer = Array<IntArray>
 
 const val DEPTH = 100
+const val DEPTH_UNDERGROUND = 40
+const val DEPTH_SKY = 60
 
 class Generator(private val width: Int, private val length: Int) {
     val biomeLayer = Array(length) {
@@ -38,10 +40,10 @@ class Generator(private val width: Int, private val length: Int) {
                             "name",
                             "description",
                             RegenLevel.NORMAL,
-                            true,
+                            false,
                             when {
-                                z < 40 -> BiomeType.UNDERGROUND
-                                z < 60 -> BiomeType.fromIndex(biomeLayer[y][x])
+                                z < DEPTH_UNDERGROUND -> BiomeType.UNDERGROUND
+                                z < DEPTH_SKY -> BiomeType.fromIndex(biomeLayer[y][x])
                                 else -> BiomeType.SKY
                             },
                             mutableListOf(),
@@ -49,6 +51,7 @@ class Generator(private val width: Int, private val length: Int) {
                             null
                         )
                     )
+                    // todo hook up exits
                     id++
                 }
             }
@@ -67,7 +70,7 @@ class Generator(private val width: Int, private val length: Int) {
         }
     }
 
-    fun createBiomeLayer(biomeCount: Int): Layer {
+    private fun createBiomeLayer(biomeCount: Int): Layer {
         val coordinates = mutableListOf<Pair<Int, Int>>()
         for (i in 1..biomeCount) {
             val w = nextInt(0, width)
@@ -76,47 +79,30 @@ class Generator(private val width: Int, private val length: Int) {
             biomeLayer[l][w] = biome
             coordinates.add(Pair(l, w))
         }
-        var filling = true
         var iteration = 0
-        while (filling) {
-            println("filling loop, iteration: $iteration")
+        while (coordinates.size > 0) {
             coordinates.removeIf {
-                val drawn = drawAround(
+                !drawAround(
                     Pair(it.first - iteration, it.second - iteration),
                     Pair(it.first + iteration, it.second + iteration),
                     biomeLayer[it.first][it.second]
                 )
-                drawn == 0
             }
             iteration++
-            filling = false
-            var unfilled = 0
-            for (l in 0 until length) {
-                for (w in 0 until width) {
-                    if (biomeLayer[l][w] == 0) {
-                        filling = true
-                        unfilled += 1
-                    }
-                }
-            }
-            println("unfilled: $unfilled")
         }
         return biomeLayer
     }
 
-    private fun drawAround(c1: Pair<Int, Int>, c2: Pair<Int, Int>, value: Int): Int {
-//        println("drawAround: $c1, $c2, $value")
-        var drawn = 0
+    private fun drawAround(c1: Pair<Int, Int>, c2: Pair<Int, Int>, value: Int): Boolean {
+        var drawn = false
         for (w in c1.first - 1..c2.first) {
             for (l in c1.second - 1..c2.second) {
                 if (isInBounds(l, w) && ((w <= c1.first || w >= c2.first) || (l <= c1.second || l >= c2.second)) && biomeLayer[l][w] == 0) {
-//                    println("filling w: $w, l: $l, value: $value")
                     biomeLayer[l][w] = value
-                    drawn++
+                    drawn = true
                 }
             }
         }
-//        println("---")
         return drawn
     }
 
