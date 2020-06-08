305a7e72679b18fecd769991d7729918fb44c2cc
Dan Munro <dan@danmunro.com>
2020-06-07 20:54:34 -0700

    add comment

--- src/test/kotlin/kotlinmud/io/service/ServerServiceTest.kt
+++ src/test/kotlin/kotlinmud/io/service/ServerServiceTest.kt
@@ -6,8 +6,13 @@ import org.junit.Test
 class ServerServiceTest {
     @Test
     fun testReadIntoBuffersIsNonBlocking() {
+        // setup
         val test = createTestService()
+
+        // when
         test.createClient()
+
+        // then -- don't block
         test.readIntoBuffers()
     }
 }
