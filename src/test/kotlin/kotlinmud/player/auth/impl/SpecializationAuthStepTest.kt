ffcb13d6b5ffffe0cbc72b39f63de3fef582313b
Dan Munro <dan@danmunro.com>
2020-08-24 07:08:51 -0700

    test specialization auth step

---
+++ src/test/kotlin/kotlinmud/player/auth/impl/SpecializationAuthStepTest.kt
@@ -0,0 +1,92 @@
+package kotlinmud.player.auth.impl
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import kotlinmud.test.TestService
+import kotlinmud.test.createTestService
+import org.junit.Test
+
+class SpecializationAuthStepTest {
+    @Test
+    fun testCanChooseWarriorSpec() {
+        // setup
+        val test = setup()
+
+        // when
+        val response = test.runPreAuth("warrior")
+
+        // then
+        assertThat(response.message).isEqualTo("ok.")
+    }
+
+    @Test
+    fun testCanChooseWarriorPartialSpec() {
+        // setup
+        val test = setup()
+
+        // when
+        val response = test.runPreAuth("war")
+
+        // then
+        assertThat(response.message).isEqualTo("ok.")
+    }
+
+    @Test
+    fun testCanChooseThiefSpec() {
+        // setup
+        val test = setup()
+
+        // when
+        val response = test.runPreAuth("thief")
+
+        // then
+        assertThat(response.message).isEqualTo("ok.")
+    }
+
+    @Test
+    fun testCanChooseClericSpec() {
+        // setup
+        val test = setup()
+
+        // when
+        val response = test.runPreAuth("cleric")
+
+        // then
+        assertThat(response.message).isEqualTo("ok.")
+    }
+
+    @Test
+    fun testCanChooseMageSpec() {
+        // setup
+        val test = setup()
+
+        // when
+        val response = test.runPreAuth("mage")
+
+        // then
+        assertThat(response.message).isEqualTo("ok.")
+    }
+
+    @Test
+    fun testCannotChooseBadSpec() {
+        // setup
+        val test = setup()
+
+        // when
+        val response = test.runPreAuth("floodle")
+
+        // then
+        assertThat(response.message).isEqualTo("that is not a specialization.")
+    }
+
+    private fun setup(): TestService {
+        return createTestService().also {
+            it.createPlayer(emailAddress)
+            setPreAuth(it)
+        }
+    }
+
+    private fun setPreAuth(test: TestService) {
+        test.setPreAuth { authStepService, player -> SpecializationAuthStep(authStepService, player) }
+    }
+}
