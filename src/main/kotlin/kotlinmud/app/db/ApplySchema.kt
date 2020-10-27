1a13baf77fddade3576c73770b014aa979dae679
Dan Munro <dan@danmunro.com>
2020-10-27 07:22:22 -0700

    don't log

--- src/main/kotlin/kotlinmud/app/db/ApplySchema.kt
+++ src/main/kotlin/kotlinmud/app/db/ApplySchema.kt
@@ -1,13 +1,10 @@
 package kotlinmud.app.db
 
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun applySchema() {
     transaction {
-        addLogger(StdOutSqlLogger)
         SchemaUtils.create(
             *getTables()
         )
