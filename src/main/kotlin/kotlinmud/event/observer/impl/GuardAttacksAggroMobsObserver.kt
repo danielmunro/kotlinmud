f86904a23dea4ca207cc3b68e61441ad7e0c4e15
Dan Munro <dan@danmunro.com>
2020-10-27 07:22:29 -0700

    use repository method

--- src/main/kotlin/kotlinmud/event/observer/impl/GuardAttacksAggroMobsObserver.kt
+++ src/main/kotlin/kotlinmud/event/observer/impl/GuardAttacksAggroMobsObserver.kt
@@ -5,12 +5,11 @@ import kotlinmud.event.impl.FightStartedEvent
 import kotlinmud.event.observer.type.Observer
 import kotlinmud.io.model.MessageBuilder
 import kotlinmud.mob.dao.MobDAO
+import kotlinmud.mob.repository.findMobsForRoom
 import kotlinmud.mob.service.MobService
-import kotlinmud.mob.table.Mobs
 import kotlinmud.mob.type.JobType
 import kotlinmud.room.dao.RoomDAO
 import kotlinx.coroutines.runBlocking
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class GuardAttacksAggroMobsObserver(private val mobService: MobService) : Observer {
@@ -37,11 +36,7 @@ class GuardAttacksAggroMobsObserver(private val mobService: MobService) : Observ
     }
 
     private fun getMobsForRoomAndNotInFight(mobService: MobService, room: RoomDAO, fight: FightStartedEvent): List<MobDAO> {
-        return MobDAO.wrapRows(
-            Mobs.select {
-                Mobs.roomId eq room.id
-            }
-        ).filter {
+        return findMobsForRoom(room).filter {
             it != fight.aggressor &&
                     it != fight.defender &&
                     it.job == JobType.GUARD &&
