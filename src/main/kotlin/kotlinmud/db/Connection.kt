07ab52280859e48f60467723b53ece4d0ba45160
Dan Munro <dan@danmunro.com>
2021-02-06 16:09:52 -0800

    try postgres?

--- src/main/kotlin/kotlinmud/db/Connection.kt
+++ src/main/kotlin/kotlinmud/db/Connection.kt
@@ -5,8 +5,8 @@ import org.jetbrains.exposed.sql.transactions.TransactionManager
 import java.sql.Connection
 
 fun createConnection() {
-    Database.connect("jdbc:sqlite:./data.db", "org.sqlite.JDBC")
-//    Database.connect("jdbc:postgresql://localhost:5432/kotlinmud", driver = "org.postgresql.Driver",
-//        user = "kotlinmud", password = "kotlinmud")
-    TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
+//    Database.connect("jdbc:sqlite:./data.db", "org.sqlite.JDBC")
+//    TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
+    Database.connect("jdbc:postgresql://localhost:5432/postgres", driver = "org.postgresql.Driver",
+        user = "postgres", password = "postgres")
 }
