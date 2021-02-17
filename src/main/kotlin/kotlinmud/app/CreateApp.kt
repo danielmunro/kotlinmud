534bc28e9af2517d7b60dc0d26c23793153a6d27
Dan Munro <dan@danmunro.com>
2021-02-16 21:14:31 -0800

    remove unused variables

--- src/main/kotlin/kotlinmud/app/CreateApp.kt
+++ src/main/kotlin/kotlinmud/app/CreateApp.kt
@@ -1,25 +1,19 @@
 package kotlinmud.app
 
-import kotlinmud.action.service.ActionService
 import kotlinmud.event.observer.type.ObserverList
 import kotlinmud.event.service.EventService
 import kotlinmud.io.service.ServerService
-import kotlinmud.mob.service.MobService
 import kotlinmud.player.auth.service.AuthStepService
 import kotlinmud.player.service.PlayerService
-import kotlinmud.time.service.TimeService
 import org.kodein.di.erased.instance
 
 fun createApp(port: Int): App {
     val container = createContainer(port)
     val eventService by container.instance<EventService>()
     val server by container.instance<ServerService>()
-    val timeService by container.instance<TimeService>()
     val observers by container.instance<ObserverList>()
     eventService.observers = observers
-    val actionService by container.instance<ActionService>()
     val playerService by container.instance<PlayerService>()
-    val mobService by container.instance<MobService>()
     val authStepService by container.instance<AuthStepService>()
     playerService.setAuthStepService(authStepService)
 
