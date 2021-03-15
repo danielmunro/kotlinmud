39db577cdc4c2164f2f79050bb5773702d1dac66
Dan Munro <dan@danmunro.com>
2021-03-15 08:09:57 -0700

    fix concurrent modification exception

--- src/main/kotlin/kotlinmud/event/observer/impl/tick/DecrementItemDecayTimerObserver.kt
+++ src/main/kotlin/kotlinmud/event/observer/impl/tick/DecrementItemDecayTimerObserver.kt
@@ -5,6 +5,7 @@ import kotlinmud.event.observer.type.Observer
 import kotlinmud.item.service.ItemService
 import kotlinmud.mob.service.MobService
 import kotlinmud.room.service.RoomService
+import kotlinx.coroutines.runBlocking
 
 class DecrementItemDecayTimerObserver(
     private val mobService: MobService,
@@ -12,8 +13,10 @@ class DecrementItemDecayTimerObserver(
     private val itemService: ItemService,
 ) : Observer {
     override suspend fun <T> invokeAsync(event: Event<T>) {
-        itemService.decrementDecayTimer()
-        roomService.removeDecayedItems()
-        mobService.removeDecayedItems()
+        runBlocking {
+            itemService.decrementDecayTimer()
+            roomService.removeDecayedItems()
+            mobService.removeDecayedItems()
+        }
     }
 }
