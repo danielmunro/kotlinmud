b2fee0658f9a0853b8deb640080e16e8e3ce0aab
Dan Munro <dan@danmunro.com>
2021-05-18 09:39:24 -0700

    incremental area building

--- src/main/kotlin/kotlinmud/world/itrias/troy/TownCenter.kt
+++ src/main/kotlin/kotlinmud/world/itrias/troy/TownCenter.kt
@@ -12,33 +12,83 @@ fun createTroyTownCenter(roomService: RoomService, connection: Room): Room {
     val roomBuilder = RoomBuilder(roomService).also {
         it.area = Area.Troy
         it.name = "The City of Troy"
+        it.description = "tbd"
     }
 
-    val room1 = build(
-        roomBuilder.also {
-            it.name = "Main Street"
-            it.description = "A well-worn cobblestone path connects the town center with the promenade. Shops line the bustling road."
-        }
-    )
-    val room2 = build(roomBuilder)
-    val room3 = build(roomBuilder)
-    val room4 = build(
+    val mainStreetBuilder = roomBuilder.copy().also {
+        it.name = "Main Street"
+        it.description = "A well-worn cobblestone path connects the town center with the promenade. Shops line the bustling road."
+    }
+
+    val walledRoad = roomBuilder.copy().also {
+        it.name = "Walled Road"
+    }
+
+    val fountainRoom = build(
         roomBuilder.copy().also {
             it.name = "A Large Fountain"
             it.description = "The center of Troy is home to a large and ornate fountain. Pristine marble wraps around the fountain, leaving a dramatic glow in the sunlight."
         }
     )
-    val room5 = build(roomBuilder)
-    val room6 = build(roomBuilder)
-    val room7 = build(roomBuilder)
 
-    connect(connection).toRoom(room1)
-        .toRoom(room2, Direction.NORTH)
-        .toRoom(room3, Direction.NORTH)
-        .toRoom(room4, Direction.NORTH)
-        .toRoom(room5, Direction.WEST)
-        .toRoom(room6, Direction.WEST)
-        .toRoom(room7, Direction.WEST)
+    val northGate = build(
+        roomBuilder.copy().also {
+            it.name = "Troy North Gate"
+            it.description = "tbd"
+        }
+    )
 
-    return room4
+    val westGate = build(
+        roomBuilder.copy().also {
+            it.name = "Troy West Gate"
+            it.description = "tbd"
+        }
+    )
+
+    val eastGate = build(
+        roomBuilder.copy().also {
+            it.name = "Troy East Gate"
+            it.description = "tbd"
+        }
+    )
+
+    connect(connection)
+        .toRoom(build(mainStreetBuilder))
+        .toRoom(build(mainStreetBuilder), Direction.NORTH)
+        .toRoom(build(mainStreetBuilder), Direction.NORTH)
+        .toRoom(fountainRoom, Direction.NORTH)
+        .toRoom(build(mainStreetBuilder), Direction.WEST)
+        .toRoom(build(mainStreetBuilder), Direction.WEST)
+        .toRoom(build(mainStreetBuilder), Direction.WEST)
+        .toRoom(westGate)
+
+    connect(fountainRoom)
+        .toRoom(build(mainStreetBuilder), Direction.EAST)
+        .toRoom(build(mainStreetBuilder), Direction.EAST)
+        .toRoom(build(mainStreetBuilder), Direction.EAST)
+        .toRoom(eastGate)
+
+    connect(fountainRoom)
+        .toRoom(build(mainStreetBuilder), Direction.NORTH)
+        .toRoom(build(mainStreetBuilder), Direction.NORTH)
+        .toRoom(build(mainStreetBuilder), Direction.NORTH)
+        .toRoom(northGate, Direction.NORTH)
+
+    connect(westGate)
+        .toRoom(build(walledRoad), Direction.NORTH)
+        .toRoom(build(walledRoad), Direction.NORTH)
+        .toRoom(build(walledRoad), Direction.NORTH)
+        .toRoom(build(walledRoad), Direction.EAST)
+        .toRoom(build(walledRoad), Direction.EAST)
+        .toRoom(build(walledRoad), Direction.EAST)
+        .toRoom(northGate, Direction.EAST)
+        .toRoom(build(walledRoad), Direction.EAST)
+        .toRoom(build(walledRoad), Direction.EAST)
+        .toRoom(build(walledRoad), Direction.EAST)
+        .toRoom(build(walledRoad), Direction.SOUTH)
+        .toRoom(build(walledRoad), Direction.SOUTH)
+        .toRoom(build(walledRoad), Direction.SOUTH)
+        .toRoom(eastGate, Direction.SOUTH)
+
+    return northGate
 }
