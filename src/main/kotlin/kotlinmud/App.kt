f73ecef74f00a836e83f4a3e4ce2bbfae672bfb9
Dan Munro <dan@danmunro.com>
2021-05-30 21:16:14 -0700

    log tweaking

--- src/main/kotlin/kotlinmud/App.kt
+++ src/main/kotlin/kotlinmud/App.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.runBlocking
 fun main() {
     val env = System.getenv("ENV") ?: "dev"
     val port = if (env == "ci") 0 else 9999
-    System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "debug")
     createConnection()
     with(createApp(port)) {
         runBlocking { startGame() }
