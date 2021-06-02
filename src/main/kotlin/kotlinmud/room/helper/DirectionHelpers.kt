28656f85325bb477197a537c852f0df2ba6cd489
Dan Munro <dan@danmunro.com>
2021-06-02 08:38:54 -0700

    unused function

--- src/main/kotlin/kotlinmud/room/helper/DirectionHelpers.kt
+++ src/main/kotlin/kotlinmud/room/helper/DirectionHelpers.kt
@@ -5,14 +5,3 @@ import kotlinmud.room.type.Direction
 fun matchDirectionString(input: String): Direction? {
     return Direction.values().find { it.value.startsWith(input) }
 }
-
-fun getDirectionString(direction: Direction): String {
-    return when (direction) {
-        Direction.DOWN -> "below you"
-        Direction.UP -> "above you"
-        Direction.EAST -> "to the east"
-        Direction.WEST -> "to the west"
-        Direction.NORTH -> "to the north"
-        Direction.SOUTH -> "to the south"
-    }
-}
